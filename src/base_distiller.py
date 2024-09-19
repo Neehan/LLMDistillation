03635d263c0fd5e2774d04a093e89afd44617c44
Adib Hasan <nzxcvb5@gmail.com>
2024-09-18 20:46:33 -0400

    increased logging freq

--- src/base_distiller.py
+++ src/base_distiller.py
@@ -108,7 +108,7 @@ class BaseDistiller:
         for epoch in range(epochs):
             losses = []
             for batch_idx, batch in enumerate(train_encodings):
-                if batch_idx == 75:
+                if batch_idx % 25 == 24:
                     logging.info(
                         f"layer {layer_id}: calculating intermediate perplexity."
                     )
