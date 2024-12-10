71ec5bda7c6bbbff74677b012e2688dd1947345f
Adib Hasan <nzxcvb5@gmail.com>
2024-12-10 11:06:03 +0600

    fixed init script

--- script.sh
+++ script.sh
@@ -30,17 +30,18 @@ then
     # Ensure conda is initialized in the script
     source ~/miniconda3/etc/profile.d/conda.sh
 fi
+# Check if CUDA is installed
+if ! command -v nvcc &> /dev/null
+then
+    # Install CUDA
+    wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
+    sudo dpkg -i cuda-keyring_1.1-1_all.deb
+    sudo apt-get update
+    sudo apt-get -y install cuda-toolkit-12-6
+fi
 
 # Create and activate a new environment
 conda create --name llm python=3.12 -y
-
-# Activate the new environment
-conda init && conda activate llm
-
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
-sudo dpkg -i cuda-keyring_1.1-1_all.deb
-sudo apt-get update
-sudo apt-get -y install cuda-toolkit-12-6
 
 # Install necessary packages
 conda activate llm && conda install pytorch torchvision -c pytorch -y
