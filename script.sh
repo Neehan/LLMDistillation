29f27b156f10172b46505ac272197b8bd47acd66
Adib Hasan <nzxcvb5@gmail.com>
2024-12-10 11:04:56 +0600

    fixed init script

--- script.sh
+++ script.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
 
+# Create necessary directories
+mkdir -p data
+mkdir -p data/datasets
+mkdir -p data/llm_cache
+
+# Create .env file
+cat > .env <<EOL
+STDOUT=1
+HALF_PRECISION=1
+TEST_ENV=1
+EOL
+
+
+
 # Check if conda is already installed
 if ! command -v conda &> /dev/null
 then
-    # Install Miniconda if conda is not found
-    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-    bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda
-    rm Miniconda3-latest-Linux-x86_64.sh
+    # install conda
+    mkdir -p ~/miniconda3
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+    bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+    rm -rf ~/miniconda3/miniconda.sh
 
-    # Add Miniconda to PATH
-    export PATH="$HOME/miniconda/bin:$PATH"
+    # initialize conda
+    ~/miniconda3/bin/conda init bash
+    ~/miniconda3/bin/conda init zsh
 
-    # Initialize conda for bash and zsh
-    conda init
+    # Ensure conda is initialized in the script
+    source ~/miniconda3/etc/profile.d/conda.sh
 fi
 
 # Create and activate a new environment
@@ -27,18 +43,6 @@ sudo apt-get update
 sudo apt-get -y install cuda-toolkit-12-6
 
 # Install necessary packages
-# conda activate llm && conda install pytorch torchvision -c pytorch -y
-conda activate llm && pip install vllm transformers accelerate python-dotenv tqdm-loggable datasets flash-attn
-
-# Create necessary directories
-mkdir -p data
-mkdir -p data/datasets
-mkdir -p data/llm_cache
-
-# Create .env file
-cat > .env <<EOL
-STDOUT=1
-HALF_PRECISION=1
-TEST_ENV=1
-EOL
-
+conda activate llm && conda install pytorch torchvision -c pytorch -y
+conda activate llm && pip install vllm 
+conda activate llm && pip install transformers accelerate python-dotenv tqdm-loggable datasets flash-attn
